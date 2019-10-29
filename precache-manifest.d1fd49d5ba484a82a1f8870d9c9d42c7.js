7458cc3ad0e5002a982c29a29a642991ee2eb4f7
Luc Mo Costabella <luc.mo_costabella@sbb.ch>
2019-10-30 00:17:44 +0100

    Added "npm run build" result from boussole

---
+++ precache-manifest.d1fd49d5ba484a82a1f8870d9c9d42c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c55a61eb35e2d6bf01df66328936a009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "373c1dba7311e0d32b9d",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "5b3f22820a544db55bf9",
+    "url": "/static/js/2.282e7bb9.chunk.js"
+  },
+  {
+    "revision": "373c1dba7311e0d32b9d",
+    "url": "/static/js/main.57cb1aef.chunk.js"
+  },
+  {
+    "revision": "4ba953727abff0fada59",
+    "url": "/static/js/runtime-main.9da1094c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
