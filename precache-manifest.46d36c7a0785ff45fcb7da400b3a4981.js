72f3eb43cd636bb0d19af8285f0067e9d0c94299
Luc Mo Costabella <luc.mo_costabella@sbb.ch>
2019-11-10 23:01:07 +0100

    Version with authentication and redirection to the original page

---
+++ precache-manifest.46d36c7a0785ff45fcb7da400b3a4981.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93f09768d56eda9c1e2308f4fa26bdd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a6bf36bcda230d50024",
+    "url": "/static/css/main.7c421f3c.chunk.css"
+  },
+  {
+    "revision": "b6ab476d076e6ce0f188",
+    "url": "/static/js/2.41dbbe7d.chunk.js"
+  },
+  {
+    "revision": "5a6bf36bcda230d50024",
+    "url": "/static/js/main.ad57b9e7.chunk.js"
+  },
+  {
+    "revision": "4ba953727abff0fada59",
+    "url": "/static/js/runtime-main.9da1094c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.962adb29bffdfa272d425e3723852075.js"
+  "/precache-manifest.46d36c7a0785ff45fcb7da400b3a4981.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
