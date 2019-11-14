c6d1811d91b37f3c8bbada817dcf66893dea1927
Luc Mo Costabella <luc.mo_costabella@sbb.ch>
2019-11-14 13:48:32 +0100

    Site de Benoît

---
+++ benoit/precache-manifest.d4e026f5e66c8a32e190da3b85c95432.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5a8e3ad02dbef1d5b62f8cfbc30742",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e596348bd6b7a6357f80",
+    "url": "/static/css/main.8cd3964f.chunk.css"
+  },
+  {
+    "revision": "63ca78e8e72b5f61f2a6",
+    "url": "/static/js/2.6c87a38a.chunk.js"
+  },
+  {
+    "revision": "e596348bd6b7a6357f80",
+    "url": "/static/js/main.00c76034.chunk.js"
+  },
+  {
+    "revision": "4ba953727abff0fada59",
+    "url": "/static/js/runtime-main.9da1094c.js"
+  }
+]);
