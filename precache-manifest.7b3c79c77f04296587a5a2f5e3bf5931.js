b7947ac9f06474a98fd755b22e6a6c223c5916e3
Luc Mo Costabella <luc.mo_costabella@sbb.ch>
2020-01-21 10:40:43 +0100

    Added missing files

---
+++ precache-manifest.7b3c79c77f04296587a5a2f5e3bf5931.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aad16ede2d60ba927a1871e8b05ad60f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fcb3b98023101621bef",
+    "url": "/static/css/main.e136269c.chunk.css"
+  },
+  {
+    "revision": "8e897cb0e2c9aed42ea5",
+    "url": "/static/js/2.f7a27187.chunk.js"
+  },
+  {
+    "revision": "0fcb3b98023101621bef",
+    "url": "/static/js/main.a408f73d.chunk.js"
+  },
+  {
+    "revision": "4ba953727abff0fada59",
+    "url": "/static/js/runtime-main.9da1094c.js"
+  }
+]);
