04bb9031d3ba6bf8f78725fb481ed7d8b93d1ea5
Luc Mo Costabella <luc.mo_costabella@sbb.ch>
2020-12-14 08:14:56 +0100

    Added missing files.

--- precache-manifest.c4e48fd356f7363ee03b0d1708df10ef.js
+++ precache-manifest.c4e48fd356f7363ee03b0d1708df10ef.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ffaca405d7349995f55ab05f88430461",
+    "revision": "9393e25f713e5fcbfede868fdf36d70d",
     "url": "/index.html"
   },
   {
-    "revision": "912918dca2dcaa413685",
-    "url": "/static/css/main.b7aeb6ac.chunk.css"
+    "revision": "09d46a7ab639834784f2",
+    "url": "/static/css/main.205d3bb4.chunk.css"
   },
   {
-    "revision": "dd5df5a778563f95eed3",
-    "url": "/static/js/2.bc4f0795.chunk.js"
+    "revision": "7639db68197f63bdfe80",
+    "url": "/static/js/2.fd594b6d.chunk.js"
   },
   {
     "revision": "85e69d7a49903bf730229457da435ef0",
-    "url": "/static/js/2.bc4f0795.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.fd594b6d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "912918dca2dcaa413685",
-    "url": "/static/js/main.b165be27.chunk.js"
+    "revision": "09d46a7ab639834784f2",
+    "url": "/static/js/main.4272e406.chunk.js"
   },
   {
     "revision": "9cca54f541ed996d544e",
